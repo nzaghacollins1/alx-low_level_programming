9bd6206b2b7baa7e04bda53233d6fcf52841d943
Nzagha Collins <nzaghacollins97@gmail.com>
2022-10-13 13:05:16 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,11 @@
 
 #include <stdio.h>
 
-/* Desciption: the prinf function prints out the the characters in it
- * Return: 0 means successful execution
+/**
+ * main - returns void
  */
-
+int main(void)
 {
-
-	int main(void)
 		printf("with proper grammar, but the outcome is a piece of art\n");
 	return (0);
 
