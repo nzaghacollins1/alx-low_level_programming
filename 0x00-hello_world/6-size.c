7a6fc583b209af47c0e8aa28fd6f04c895e763ec
Nzagha Collins <nzaghacollins97@gmail.com>
2022-10-13 13:54:05 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,12 @@
  */
 int main(void)
 {
+	char a;
+	int b;
+	long int c;
+	lon long int d;
+	float e;
+
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
