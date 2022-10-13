9c14927757a76db8b9b8c35281797a0ac7712a79
Nzagha Collins <nzaghacollins97@gmail.com>
2022-10-13 14:20:49 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#include <unistd.h>
+
+/**
+  * main - prints text
+  *
+  * return: 1
+  */
+int main(void)
+{
+
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2025-10-19\n")
+    return (1);
+
+}
