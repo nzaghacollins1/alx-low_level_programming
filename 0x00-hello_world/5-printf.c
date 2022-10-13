42421edb13db93e6825ef3cd6202ed0c914189e3
Nzagha Collins <nzaghacollins97@gmail.com>
2022-10-13 12:49:45 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+# !/ bin / bash
+
+#include <stdio.h>
+
+/* Desciption: the prinf function prints out the the characters in it
+ * Return: 0 means successful execution
+ */
+
+{
+
+	int main(void)
+		printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+
+}
