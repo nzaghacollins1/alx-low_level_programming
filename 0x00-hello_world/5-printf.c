44926fa847bbe4df6402a131d38e967df7481c84
Nzagha Collins <nzaghacollins97@gmail.com>
2022-10-13 17:27:21 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
 /**
  * main - prints text to the screen
  *
- *Retuen: 0 if exited properly
+ *Return: 0 if exited properly
  */
-int main(void)
+int main(print text to screen)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
