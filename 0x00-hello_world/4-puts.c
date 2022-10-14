c9146c5ce8d110c311b36937c65d131f2904ee9f
Nzagha Collins <nzaghacollins97@gmail.com>
2022-10-13 17:35:47 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,15 +3,12 @@
 /*
  * Description: Run puts and print statement
  * Return: Return 0 to exit function
- */
-
-int main(void)
-
-/*
+ *
  * main - initialization point
  */
 
+int main(void)
 {
-	puts(" \"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
