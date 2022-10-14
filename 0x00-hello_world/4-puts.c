b5a2512bb084829347b69c6c0190a7cf6b62d3d9
Nzagha Collins <nzaghacollins97@gmail.com>
2022-10-13 17:45:40 -0700

    puts for printing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,13 @@
 
 /*
  * Description: Run puts and print statement
- * Return: Return 0 to exit function
+ *
+ * Return: Return: 0 to exit function
  *
  * main - initialization point
  */
 
-int main(initialization)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
