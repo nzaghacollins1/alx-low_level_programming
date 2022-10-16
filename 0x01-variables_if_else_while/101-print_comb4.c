a122610428cf83a3b05b23f6ea69c5f730e42f2f
Nzagha Collins <nzaghacollins97@gmail.com>
2022-10-16 15:28:23 -0700

    print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/*
+ * main: prints all possoble combination of two two numbers
+ *
+ * Return: 0, successful
+ */
+int main(void)
+{
+	int a, b , c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+
+					if (a != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
