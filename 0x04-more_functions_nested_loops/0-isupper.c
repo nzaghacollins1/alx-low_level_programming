97b9ea23c3cf266ddec438dfdb2a0487b444a64c
Nzagha Collins <nzaghacollins97@gmail.com>
2022-10-20 15:33:10 -0700

    check if uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - function verifies if character is uppercase
+ * @c: int value
+ * Return: 1 if c is uppercase or 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
