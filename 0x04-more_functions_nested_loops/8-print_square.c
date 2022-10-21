69e19d40bc0aa2b7781817a20031cbd9645026ec
Nzagha Collins <nzaghacollins97@gmail.com>
2022-10-20 21:10:16 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line
+ * @size: integer
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int 1, i;
+
+	if (size <= 0)
+		_putchar(10);
+
+	for (1 = 0; 1 < size; 1++)
+	{
+		for (i = 0; i < size; a++)
+			_putchar('#');
+		_putchar(10);
+	}
+}
