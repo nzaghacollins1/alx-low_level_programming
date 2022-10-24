b94a1112d83e8e43bdfb1dd4889b848538ed16db
Nzagha Collins <nzaghacollins97@gmail.com>
2022-10-24 14:43:14 -0700

    print a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+ *_puts - print a string with new line
+ *@str: pointer to string
+ *
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
