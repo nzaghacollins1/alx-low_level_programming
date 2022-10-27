5951f1ab9dcf248bab97e2f8357b3a093a076291
Nzagha Collins <nzaghacollins97@gmail.com>
2022-10-27 15:29:26 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,19 +2,34 @@
 
 /**
  *cap_string - capitalizes all strings
- *@i: string to return
+ *@j: string to return
  *
- *Return: @i
+ *Return: caps on first letter
  */
-char *cap_string(char *i)
+char *cap_string(char *j)
 {
-	int j;
+	int i, x;
+	int cap = 32;
+	int separators[] = {',', ';', '.', '?', '"',
+			    '(', ')', '{', '}', ' ', '\n', '\t'};
 
-	for (j = 0; i[j] != '\0'; j++)
+	for (i = 0; j[i] != '\0'; i++)
 	{
-		if (i[j] >= 'a' && i[j] <= 'z')
-			i[j] = i[j] - 32;
-	}
+		if (j[i] >= 'a' && j[i] <= 'z')
+		{
+			j[i] = j[i] - cap;
+		}
 
-	return (i);
+		cap = 0;
+
+		for (x = 0; x <= 12; x++)
+		{
+			if (j[i] == separators[x])
+			{
+				x = 12;
+				cap = 32;
+			}
+		}
+	}
+	return (j);
 }
